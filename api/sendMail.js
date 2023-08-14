9c9f9643577c97128cc3b8ca48ef11dff79d4e15
dayo4 <dayo4@live.com>
2023-08-14 20:49:15 +0100

    .

--- api/sendMail.js
+++ api/sendMail.js
@@ -1,27 +1,7 @@
 require("dotenv").config();
 
-const corsHandler = (handler) => async (req, res) => {
-    res.setHeader("Access-Control-Allow-Credentials", true);
-    res.setHeader("Access-Control-Allow-Origin", "https://orbrift.com/");
-    res.setHeader(
-      "Access-Control-Allow-Methods",
-      "GET,OPTIONS,PATCH,DELETE,POST,PUT"
-    );
-    res.setHeader(
-      "Access-Control-Allow-Headers",
-      "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version"
-    );
-  
-    if (req.method === "OPTIONS") {
-      res.status(200).end();
-      return;
-    }
-  
-    return await handler(req, res);
-  };
-  
 const nodeMailer = require("nodemailer");
-const { /* corsHandler, */ reCaptcha, sanitizeHTML } = require("../plugins");
+const { corsHandler, reCaptcha, sanitizeHTML } = require("../plugins");
 
 const transport = nodeMailer.createTransport({
   //@ts-ignore
